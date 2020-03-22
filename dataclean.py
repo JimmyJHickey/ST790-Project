ce21403b49e3d657a088d6f1ff78b8719d0c7f91
David Elsheimer <37280299+davelsh@users.noreply.github.com>
2020-03-21 22:37:15 -0400

    Data slicing python code

---
+++ dataclean.py
@@ -0,0 +1,12 @@
+import pandas as pd
+train = pd.read_csv('Kannada-MNIST/train.csv')
+test = pd.read_csv('Kannada-MNIST/test.csv')
+print(train)
+trainY = train[train.columns[0]]
+print(trainY)
+trainX = train[train.columns[1:]]
+print(trainX)
+testY = test[test.columns[0]]
+print(testY)
+testX = test[test.columns[1:]]
+print(testX)
