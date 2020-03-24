155e547a045815f08bba4f123050d35c53534d6a
jimmyjhickey <jimmyjhickey@gmail.com>
2020-03-24 16:50:39 -0400

    Added generator shape as input

--- src/GAN/base_gan.py
+++ src/GAN/base_gan.py
@@ -5,24 +5,24 @@ import numpy as np
 
 class GAN:
 
-    def __init__(self, g_gen=None):
+    def __init__(self, gen_input_shape, g_gen=None):
         """
 
         :param g_gen: (Optional) Wrapper function for G (generator) random input sample
             This will default to a Uniform [-1, 1] generator.
         """
-
+        self.gen_input_shape = gen_input_shape
         # Set default random generator to uniform [-1, 1]
         if g_gen is None:
             self.g_gen = lambda s: np.random.uniform(-1.0, 1.0, size=s)
         else:
             self.g_gen = g_gen
 
-    def g_sample(self, size):
+    def g_sample(self):
         """
         g_sample
         Random sample input for generator.
         :param size: number of samples to generate
         :return: size randomly generated numbers
         """
-        return self.g_gen(size)
+        return self.g_gen(self.gen_input_shape)
