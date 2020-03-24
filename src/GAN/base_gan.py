993c4189df30c3d477cea08d471dc8d4f6939d73
jimmyjhickey <jimmyjhickey@gmail.com>
2020-03-24 14:18:28 -0400

    added generator sample method

--- src/GAN/base_gan.py
+++ src/GAN/base_gan.py
@@ -1,12 +1,29 @@
-#import tensorflow as tf
-#import numpy as np
-#import os
+# import tensorflow as tf
+import numpy as np
+# import keras
+
 
 class GAN:
 
-    def __init__(self):
+    def __init__(self, g_gen = None):
+        """
+
+        :param g_gen: (Optional) Wrapper function for G (generator) random input sample
+            This will default to a Uniform [-1, 1] generator.
+        """
         self.name = "test"
 
+        # Set default random generator to uniform [-1, 1]
+        if g_gen is None:
+            self.g_gen = lambda s: np.random.uniform(-1.0, 1.0, size=s)
+        else:
+            self.g_gen = g_gen
 
-g = GAN()
-print(g.name)
+    def g_sample(self, size):
+        """
+        g_sample
+        Random sample input for generator.
+        :param size: number of samples to generate
+        :return: size randomly generated numbers
+        """
+        return self.g_gen(size)
